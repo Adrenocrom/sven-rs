18c5daa3238d405a76018ae3d1b96f2597276425
Josef Schulz <j.schulz@alaun.de>
2026-09-11 00:27:30 +0200

    u

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
-use std::{collections::HashMap, error::Error, slice::SliceIndex};
+use std::{collections::HashMap, error::Error, slice::SliceIndex, string::FromUtf8Error};
 
 use schemars::{JsonSchema, schema_for};
 use serde::Deserialize;
 use serde_json::{Value, json};
+use thiserror::Error;
 
 trait Tool {
     fn name(&self) -> String;
     fn description(&self) -> String;
     fn params(&self) -> Option<Value>;
     fn execute(&self, params: Value) -> Result<String, Box<dyn Error>>;
+}
+
+pub enum ToolError {
+    MissingParameter,
+}
+
+impl From<FromUtf8Error> for ToolError {
+    fn from(value: FromUtf8Error) -> Self {
+        ToolError::MissingParameter
+    }
 }
 
 macro_rules! define_tool {
@@ -26,6 +37,27 @@ macro_rules! define_tool {
 
         impl Tool for $tool {
             fn name(&self) -> String { String::from($name) }
+            fn description(&self) -> String { String::from($desc) }
+            fn params(&self) -> Option<Value> {
+                Some(json!(schema_for!($params)))
+            }
+            fn execute(&self, params: Value) -> Result<String, Box<dyn Error>> {
+                let $args: $params = serde_json::from_value(params)?;
+                $($body)*
+            }
+        }
+    };
+}
+
+macro_rules! define_tool2 {
+    (
+        $tool:ident, $params:ident, $desc:literal,
+        execute($args:ident) { $($body:tt)* }
+    ) => {
+        struct $tool;
+
+        impl Tool for $tool {
+            fn name(&self) -> String { String::from(stringify!($tool)) }
             fn description(&self) -> String { String::from($desc) }
             fn params(&self) -> Option<Value> {
                 Some(json!(schema_for!($params)))
@@ -86,15 +118,28 @@ define_tool!(ManTool, ManToolParams, "man_tool", "display man pages",
     }
 );
 
+#[derive(Deserialize, JsonSchema)]
+struct WeatherToolParams {
+    ///  city name for the weather prediction
+    city: String
+}
+define_tool2!(WeatherTool, WeatherToolParams, "show current weather conditions", execute(args) {
+    Ok(format!("current weather in {}: {}", args.city, "sunny".to_string())) 
+});
+
 fn main() {
-    let tools = tools![ReadTool, WriteTool, ManTool];
+    let tools = tools![WeatherTool, ReadTool, WriteTool, ManTool];
+    println!("{}", stringify!(WeatherTool));
     
-    let r_t = match tools.get("write_tool") {
+    let r_t = match tools.get("WeatherTool") {
         Some(t) => t,
         None => return
     };
 
-    let result = match r_t.execute(json!({"path":"example.txt", "content": "Hello World"})) {
+    let p = r_t.params().expect("some");
+    println!("params: {}", p.to_string());
+
+    let result = match r_t.execute(json!({"city":"berlin"})) {
         Ok(res) => res,
         Err(e) => e.to_string()
     };
