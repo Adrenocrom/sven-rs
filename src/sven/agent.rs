04792831ca25e76db3153ac19f5a323c5ecc2b48
Josef Schulz <j.schulz@alaun.de>
2026-09-16 09:47:32 +0200

    u

--- src/sven/agent.rs
+++ src/sven/agent.rs
@@ -50,7 +50,7 @@ impl StreamState {
             let Some(eval_count) = json["eval_count"].as_u64() && 
             let Some(prompt_eval_count) = json["prompt_eval_count"].as_u64()
         {
-            print!("\n\x1b[1min {} out {} | used (-/-)\x1b[0m", prompt_eval_count, eval_count);
+            print!("\n\x1b[1min {} out {} | used (-/-)\x1b[0m\n", prompt_eval_count, eval_count);
         }
 
         if let Some(tcs) = json["message"]["tool_calls"].as_array() {
